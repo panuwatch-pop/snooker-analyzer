67a5481c93648ab4fe467e52be1f038c12169188
panuwatch-pop <panuwat.ch@gmail.com>
2026-09-02 23:03:02 +0700

    Auto purge cache storage and force unregister stale service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Unregister stale service workers and clear cache storage
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+  if ('caches' in window) {
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    });
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
